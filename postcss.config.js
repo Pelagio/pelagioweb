798fd95b7a7b676d3f8987dd2b5bb9384dfc2635
Simon Arneson <simon@pelag.io>
2019-05-09 21:03:05 +0200

    Fix css for people section

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
-const postcssImport = require(`postcss-import`)
-const postcssCssNext = require(`postcss-cssnext`)
-const postcssBrowserReporter = require(`postcss-browser-reporter`)
-const postcssReporter = require(`postcss-reporter`)
+const postcssImport = require(`postcss-import`);
+const postcssCssNext = require(`postcss-cssnext`);
+const postcssBrowserReporter = require(`postcss-browser-reporter`);
+const postcssReporter = require(`postcss-reporter`);
 
 module.exports = () => ({
   plugins: [
     postcssImport(),
     postcssCssNext(),
     postcssBrowserReporter(),
-    postcssReporter(),
-  ],
-})
+    postcssReporter()
+  ]
+});
