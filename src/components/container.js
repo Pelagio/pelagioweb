b7987ff00a99d580c3a3e25b8fe26f86323bf4bc
Simon Arneson <simon@pelag.io>
2019-05-24 11:54:26 +0200

    Fix vertical scrolling bug

--- src/components/container.js
+++ src/components/container.js
@@ -1,3 +1,5 @@
 import React from "react";
 
-export default ({ children }) => <div style={null}>{children}</div>;
+export default ({ children }) => (
+  <div style={{ overflowX: "hidden" }}>{children}</div>
+);
