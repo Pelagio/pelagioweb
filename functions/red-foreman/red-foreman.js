9dbab51778e33814568c3058c122cf31a37dacf0
Simon Arneson <simon@pelag.io>
2019-11-14 14:09:38 +0100

    Set content type header

--- functions/red-foreman/red-foreman.js
+++ functions/red-foreman/red-foreman.js
@@ -5,11 +5,14 @@ exports.handler = function(event, context, callback) {
   console.log({
     randomIndex,
     quote: quotes[randomIndex],
-    length: quotes.length
+    length: quotes.length,
+    event,
+    context
   });
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(quotes[randomIndex])
+    body: JSON.stringify(quotes[randomIndex]),
+    headers: { "Content-Type": "application/json" }
   });
 };
