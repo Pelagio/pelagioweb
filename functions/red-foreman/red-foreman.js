d1a62cf41d582db8b835121447bbc13f6cf54790
Simon Arneson <simon@pelag.io>
2019-11-14 14:18:12 +0100

    Readd some logging

--- functions/red-foreman/red-foreman.js
+++ functions/red-foreman/red-foreman.js
@@ -5,7 +5,7 @@ exports.handler = function(event, context, callback) {
   const quotesToUse =
     oneLiner === "true" ? quotes.filter(quotes => quotes.length === 1) : quotes;
   const randomIndex = Math.floor(Math.random() * quotesToUse.length - 1);
-
+  console.log({ quotesToUse, randomIndex });
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(quotesToUse[randomIndex]),
