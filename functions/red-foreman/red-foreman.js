bae7f00c60d3054be4bfc28524c7af87a4155856
Simon Arneson <simon@pelag.io>
2019-11-14 13:54:33 +0100

    Add logging

--- functions/red-foreman/red-foreman.js
+++ functions/red-foreman/red-foreman.js
@@ -2,6 +2,7 @@ const quotes = require("./quotes");
 
 exports.handler = function(event, context, callback) {
   const randomIndex = Math.floor(Math.random() * quotes.length - 1);
+  console.log({ randomIndex, quote: quotes[randomIndex] });
 
   callback(null, {
     statusCode: 200,
