c2b74cf68d813c0f44848e18b7945717ab1d0918
Lukas Suter <lukas.suter@swegon.com>
2020-06-28 14:55:03 +0200

    Added files for navigation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,9 @@
 import React from "react";
-import base from "./base.css";
 import Container from "../components/container";
-import Navigation from "../components/navigation";
 
 class Template extends React.Component {
   render() {
-    const { location, children } = this.props;
+    const { children } = this.props;
 
     return <Container>{children}</Container>;
   }
