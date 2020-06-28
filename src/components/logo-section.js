f84247e19dd6394b17553a07fbc2f16085485895
Erik Nordmark <nordmark@Eriks-MacBook-Pro-2.local>
2020-06-28 18:49:12 +0200

    Fixed wave background

--- src/components/logo-section.js
+++ src/components/logo-section.js
@@ -3,12 +3,15 @@ import React from "react";
 import sectionStyles from "./page-section.module.css";
 import styles from "./logo-section.module.css";
 
+import logo from "../../static/img/logo.svg";
+
 export default ({ section }) => {
   return (
     <section id="title" className={sectionStyles.sectionBlock}>
       <div className={styles.sectionContent}>
-        <a className={styles.headerMenuCenter} href="/" />
-        <div className={styles.spacer} />
+        <a href="/">
+          <img src={logo} alt="Logo" />
+        </a>
       </div>
     </section>
   );
