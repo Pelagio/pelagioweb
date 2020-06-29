3b230091fd8576c43e4d984d3ebfff1b41d587ad
Simon Arneson <simon@pelag.io>
2020-06-29 14:33:41 +0200

    Support delay in anchorscroll.

--- src/helpers/anchor-scroll.js
+++ src/helpers/anchor-scroll.js
@@ -1,10 +1,15 @@
-export const anchorScroll = e => {
-  let hashval = e.target.getAttribute("href");
-  let target = document.querySelector(hashval);
-  target.scrollIntoView({
-    behavior: "smooth",
-    block: "start"
-  });
-  window.history.pushState(null, null, hashval);
+export const anchorScroll = (e, delay = 0) => {
   e.preventDefault();
+  const t = e.target;
+  const scroll = () => {
+    let hashval = t.getAttribute("href");
+    let target = document.querySelector(hashval);
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+    window.history.pushState(null, null, hashval);
+  };
+
+  setTimeout(scroll, delay);
 };
