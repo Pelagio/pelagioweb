fce165d5d8e8d30ac41113e9ef4ce2fc2a0dea9c
Simon Arneson <simon@pelag.io>
2021-11-26 13:38:42 +0100

    Add call to aoc backend.

--- functions/advent-of-code/advent-of-code.js
+++ functions/advent-of-code/advent-of-code.js
@@ -1,7 +1,20 @@
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ disIsData: "Frank är sämst" }),
-    headers: { "Content-Type": "application/json" }
+const fetch = require("node-fetch");
+
+exports.handler = async function(event, context, callback) {
+  return new Promise(async (resolve, reject) => {
+    const data = await (await fetch(
+      "https://adventofcode.com/2021/leaderboard/private/view/1065422.json",
+      {
+        headers: {
+          cookie: `session=${process.env.AOC_SESSION};`
+        }
+      }
+    )).text();
+    console.log({ data });
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    });
   });
 };
