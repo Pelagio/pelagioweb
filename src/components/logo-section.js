0321ed37d168c54db3280b52b34393f3d932ec6f
Simon Arneson <simon@pelag.io>
2026-02-10 15:33:18 +0100

    Make scroll indicator clickable with smooth scroll to next section

--- src/components/logo-section.js
+++ src/components/logo-section.js
@@ -5,16 +5,27 @@ import styles from "./logo-section.module.css";
 import logo from "../../static/img/logo-outset-white.svg";
 
 export default ({ section }) => {
+  const handleScrollDown = () => {
+    window.scrollTo({
+      top: window.innerHeight,
+      behavior: "smooth"
+    });
+  };
+
   return (
     <section id="title">
       <div className={styles.sectionContent}>
         <div className={styles.logoWrapper}>
           <img src={logo} alt="Pelagio - Software Development Agency" />
         </div>
-        <div className={styles.scrollIndicator}>
+        <button
+          className={styles.scrollIndicator}
+          onClick={handleScrollDown}
+          aria-label="Scroll to next section"
+        >
           <span>Scroll</span>
           <div className={styles.scrollArrow} />
-        </div>
+        </button>
       </div>
     </section>
   );
